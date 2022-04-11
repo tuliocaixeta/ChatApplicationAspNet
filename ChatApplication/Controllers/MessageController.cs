b5e3d13af752e8c1d9b5ceea15e9a7616292e4f6
tulio <tulio@DESKTOP-T85N6RV>
2022-04-11 10:12:37 -0300

    cleaning remaining files

--- ChatApplication/Controllers/MessageController.cs
+++ ChatApplication/Controllers/MessageController.cs
@@ -1,12 +1,6 @@
 #nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ChatApplication.Data;
 using Microsoft.AspNetCore.Authorization;
 using Services.Services.interfaces;
 using Repositories.Models;
